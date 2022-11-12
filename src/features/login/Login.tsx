1e85bcc0b5737275ca36486dec31b175b837265a
splinshady <splin.shady@gmail.com>
2022-11-12 21:38:20 +0300

    feat: add form

--- src/features/login/Login.tsx
+++ src/features/login/Login.tsx
@@ -4,35 +4,71 @@ import Checkbox from '@mui/material/Checkbox';
 import FormControl from '@mui/material/FormControl';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormGroup from '@mui/material/FormGroup';
-import FormLabel from '@mui/material/FormLabel';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import {useFormik} from "formik";
 
 export const Login = () => {
-    return <Grid container justifyContent={'center'}>
-        <Grid item justifyContent={'center'}>
-            <FormControl>
-                <FormLabel>
-                    <p>To log in get registered
-                        <a href={'https://social-network.samuraijs.com/'}
-                           target={'_blank'}> here
-                        </a>
-                    </p>
-                    <p>or use common test account credentials:</p>
-                    <p>Email: free@samuraijs.com</p>
-                    <p>Password: free</p>
-                </FormLabel>
-                <FormGroup>
-                    <TextField label="Email" margin="normal"/>
-                    <TextField type="password" label="Password"
-                               margin="normal"
-                    />
-                    <FormControlLabel label={'Remember me'} control={<Checkbox/>}/>
-                    <Button type={'submit'} variant={'contained'} color={'primary'}>
-                        Login
-                    </Button>
-                </FormGroup>
-            </FormControl>
-        </Grid>
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+      rememberMe: false
+    },
+    validate: (values) => {
+      const errors: FormikErrorType = {}
+      if (!values.email) {
+        errors.email = 'Required'
+      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email address'
+      }
+      if (!values.password) {
+        errors.password = 'Required'
+      } else if (values.password.length < 3) {
+        errors.password = 'Password length should be more then 3 '
+      }
+      return errors
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values));
+      formik.resetForm()
+    },
+  })
+
+  return <Grid container justifyContent={'center'}>
+    <Grid item justifyContent={'center'}>
+      <form onSubmit={formik.handleSubmit}>
+        <FormControl>
+          <FormGroup>
+
+            <TextField label="Email"
+                       margin="normal"
+                       {...formik.getFieldProps('email')}/>
+            {formik.touched.email && formik.errors.email ? <div>{formik.errors.email}</div> : null}
+
+            <TextField type="password"
+                       label="Password"
+                       margin="normal"
+                       {...formik.getFieldProps('password')}/>
+            {formik.touched.password && formik.errors.password ? <div>{formik.errors.password}</div> : null}
+
+            <FormControlLabel label={'Remember me'}
+                              control={<Checkbox/>}
+                              checked={formik.values.rememberMe}
+                              {...formik.getFieldProps('rememberMe')}/>
+            <Button type={'submit'} variant={'contained'} color={'primary'}>
+              Login
+            </Button>
+          </FormGroup>
+        </FormControl>
+      </form>
     </Grid>
+  </Grid>
+}
+
+type FormikErrorType = {
+  email?: string
+  password?: string
+  rememberMe?: boolean
 }
